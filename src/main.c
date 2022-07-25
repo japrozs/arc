ccc6b68a0d9dd91995b2ebff2339ea495295619d
japrozs <sainijaproz@gmail.com>
2022-07-25 23:02:59 +0530

    :tada: create `symbol_table` functionality

--- src/main.c
+++ src/main.c
@@ -1,5 +1,6 @@
 #include "arc.h"
 #include "lexer.h"
+#include "sym_table.h"
 #include "parser.h"
 #include "llvm_cfg.h"
 
@@ -33,7 +34,10 @@ int main(int argc, char **argv)
 	init_llvm(argv[1]); // setup LLVM stuff
 	lexer_t *lexer = init_lexer(code);
 	parser_t *parser = init_parser(lexer);
+
+	init_table(); // initialize symbol_table
 	parser_parse(parser);
+	// print_table();
 	dispose_llvm(); // dispose and free allocated memory for the LLVM compiler
 	free(parser);
 	free(lexer);
--- src/sym_table.c
+++ src/sym_table.c
@@ -1,4 +1,69 @@
 #include "sym_table.h"
 
-void init_table() {}
-void set_symbol(symbol_type type, char *name, LLVMValueRef value) {}
+symbol_t *symbols[TABLE_SIZE];
+
+int hash(char *name)
+{
+	int length = strlen(name);
+	int hash_value = 0;
+	for (int i = 0; i < length; i++)
+	{
+		hash_value += name[i];
+		hash_value = (hash_value * name[i]) % TABLE_SIZE;
+	}
+
+	return hash_value;
+}
+
+void init_table()
+{
+	for (int i = 0; i < TABLE_SIZE; i++)
+	{
+		symbols[i] = NULL;
+	}
+}
+
+void print_table()
+{
+	for (int i = 0; i < TABLE_SIZE; i++)
+	{
+		if (symbols[i] == NULL)
+		{
+			printf("\t%d\t---\t---\n", i);
+		}
+		else
+		{
+			printf("\t%d\t%d\t%s\n", i, symbols[i]->type, symbols[i]->name);
+		}
+	}
+}
+
+symbol_t *get_symbol(char *name)
+{
+	int idx = hash(name);
+	if (symbols[idx] != NULL && strcmp(symbols[idx]->name, name) == 0)
+	{
+		return symbols[idx];
+	}
+	else
+	{
+		return NULL;
+	}
+}
+
+void set_symbol(symbol_type type, char *name, LLVMValueRef value)
+{
+	int key = hash(name);
+	while (symbols[key] != NULL)
+	{
+		// TODO: return a err status code if unable to set
+		// 		 a symbol
+		return;
+	}
+	symbol_t *sym = malloc(sizeof(symbol_t));
+	sym->type = type;
+	sym->name = malloc(sizeof(char) * strlen(name));
+	strcpy(sym->name, name);
+	sym->value = value;
+	symbols[key] = sym;
+}
